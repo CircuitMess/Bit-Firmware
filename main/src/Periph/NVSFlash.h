826d8f6fa4c903bd0db28c220b63d122f995bc5b
Fr0zen3k <jan.kresic@circuitmess.com>
2024-04-23 15:11:03 +0200

    Added array operations to NVS flash abstraction

--- main/src/Periph/NVSFlash.h
+++ main/src/Periph/NVSFlash.h
@@ -4,6 +4,7 @@
 #include <nvs.h>
 #include <string>
 #include <esp_log.h>
+#include <array>
 
 class NVSFlash {
 public:
@@ -26,6 +27,37 @@ public:
 	template<typename T>
 	bool set(const std::string& blob, const T& data) const{
 		esp_err_t err = nvs_set_blob(handle, blob.c_str(), &data, sizeof(T));
+
+		if(err != ESP_OK){
+			ESP_LOGW(TAG, "NVS blob %s store error: %d", blob.c_str(), err);
+			return false;
+		}
+
+		err = nvs_commit(handle);
+		if(err != ESP_OK){
+			ESP_LOGW(TAG, "NVS blob %s commit error: %d", blob.c_str(), err);
+			return false;
+		}
+
+		return true;
+	}
+
+	template<typename T, size_t S>
+	bool get(const std::string& blob, std::array<T, S>& dataArray) const requires(S > 0){
+		size_t out_size = sizeof(T) * S;
+		auto err = nvs_get_blob(handle, blob.c_str(), dataArray.data(), &out_size);
+
+		if(err != ESP_OK){
+			ESP_LOGI(TAG, "Blob %s not found", blob.c_str());
+			return false;
+		}
+
+		return true;
+	}
+
+	template<typename T, size_t S>
+	bool set(const std::string& blob, const std::array<T, S>& dataArray) const requires(S > 0){
+		esp_err_t err = nvs_set_blob(handle, blob.c_str(), dataArray.data(), sizeof(T) * S);
 
 		if(err != ESP_OK){
 			ESP_LOGW(TAG, "NVS blob %s store error: %d", blob.c_str(), err);
