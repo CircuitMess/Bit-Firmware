36e09641b5de981db2bbfef2ec072e2a1111e7cb
Emil Gajšak <emil.gajsak@gmail.com>
2024-06-24 15:03:35 +0200

    Fixed merge between MenuHeaderText/Foreground styles

--- main/src/Screens/MainMenu/MenuHeader.cpp
+++ main/src/Screens/MainMenu/MenuHeader.cpp
@@ -24,7 +24,7 @@ MenuHeader::MenuHeader(lv_obj_t* parent) : LVObject(parent){
 	lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
 	lv_obj_set_size(label, 27, 7);
 	lv_obj_set_style_text_font(label, &landerfont, 0);
-	lv_obj_set_style_text_color(label, THEMED_COLOR(MenuHeaderText, settings->get().theme), 0);
+	lv_obj_set_style_text_color(label, THEMED_COLOR(Foreground, settings->get().theme), 0);
 
 	auto* xpSystem = (XPSystem*) Services.get(Service::XPSystem);
 	auto lvl = xpSystem->getLevel();
